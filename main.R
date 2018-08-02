2f6e0a719eca63fffbfb985da75afd7877b10b93
wangyazhu <wangyazhu@dankegongyu.com>
2018-08-02 10:40:08 +0800

    添加提示

--- main.R
+++ main.R
@@ -45,31 +45,11 @@ result <- purrr::map(samples,  ~ {
     #`[[`(1)
 })
 
-read_api_config <- function(which_api){
-  
-  # make sure file exists
-  if(!file.exists("apiconfig.yaml")){
-    stop("Require config file - apiconfig.yaml")
-  }
-  
-  # read config
-  d <- read_yaml("apiconfig.yaml")
-  
-  # check if the credentials are specified
-  if(!all(c("host", "port", "swagger") %in% names(d[[which_api]]))){
-    stop(paste("One or more parameter in", which_api, "is missing."))
-  }
-  
-  # return endpoint
-  endpoint = with(d[[which_api]], paste0(host, ":", port, "/", swagger))
-  
-  # return 
-  endpoint
-}
+cat_rule("Exported result.", col = "lightblue")
 
 read_api_config("validator") %>% POST()
 
-
+cat_rule("Mission complete.", col = "green") 
 
 
 
